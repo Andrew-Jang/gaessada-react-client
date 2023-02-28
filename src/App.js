4485c6db8a6a865efd0e7e03c893a7e62c81eba6
Andrew Jang <dhj9817@gmail.com>
2023-02-28 17:05:07 +0900

    .

--- src/App.js
+++ src/App.js
@@ -6,29 +6,11 @@ import TermsPage from "./pages/TermsPage";
 import Privacy from "./pages/Privacy";
 import Legal from "./pages/Legal";
 import PaymentTerms from "./pages/PaymentTerms";
-import DownloadApp from "./pages/DownloadApp";
-import Withdraw from "./pages/Withdraw";
-import ZelleDetails from "./pages/ZelleDetails";
-import PaypalDetails from "./pages/PaypalDetails";
-import Waitlist from "./pages/Waitlist";
-import Event from "./pages/Event";
-import SignIn from "./pages/SignIn";
-import CreateEvent from "./pages/CreateEvent";
-import VerifyTicket from "./pages/VerifyTicket";
 
 function App() {
   return (
     <Routes>
       <Route path="/*" element={<MainPage />} />
-      <Route path="/event" element={<Event />} />
-      <Route path="/create" element={<CreateEvent />} />
-      <Route path="/verify_ticket" element={<VerifyTicket />} />
-      <Route path="/login" element={<SignIn />} />
-      <Route path="/download" element={<DownloadApp />} />
-      <Route path="/waitlist" element={<Waitlist />} />
-      <Route path="/withdraw" element={<Withdraw />} />
-      <Route path="/withdraw/zelle" element={<ZelleDetails />} />
-      <Route path="/withdraw/paypal" element={<PaypalDetails />} />
       <Route path="legal/" element={<Legal />} />
       <Route path="legal/terms-of-use" element={<TermsPage />} />
       <Route path="legal/privacy-policy" element={<Privacy />} />
